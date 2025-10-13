2c07921d70dc8889fc3d50bfa3fa416a17756496
Shiv Yadav <shivyadav0014@gmail.com>
2025-10-13 22:12:17 +0545

    add starting components for the weather app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-// import './App.css'
-import WeatherApp from "./WeatherApp"
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
+import WeatherApp from "./WeatherApp";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function App() {
-
   return (
     <>
-     <WeatherApp/>
+      <WeatherApp />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
