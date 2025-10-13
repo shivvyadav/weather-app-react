927018538f0583bf8d0fc0d13e67ab3d5d13b1ff
Shiv Yadav <shivyadav0014@gmail.com>
2025-10-13 22:14:16 +0545

    add title informations

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,20 +1,19 @@
 import SearchBox from "./SearchBox";
 import InfoBox from "./InfoBox";
-import { useState } from "react";
+import {useState} from "react";
 
 export default function WeatherApp() {
+  const [weatherInfo, setWeatherInfo] = useState({});
 
-    const [weatherInfo, setWeatherInfo] = useState({});
+  let updateWeatherInfo = (result) => {
+    setWeatherInfo(result);
+  };
 
-    let updateWeatherInfo=(result)=>{
-        setWeatherInfo(result);
-    }
-
-    return (
-        <div style={{textAlign:"center"}}>
-            <h1>Weather App by Shiv</h1>
-            <SearchBox updateWeatherInfo={updateWeatherInfo} />
-            <InfoBox info={weatherInfo}/>
-       </div>
-    );
+  return (
+    <div style={{textAlign: "center"}}>
+      <h1>Weather App by Shiv</h1>
+      <SearchBox updateWeatherInfo={updateWeatherInfo} />
+      <InfoBox info={weatherInfo} />
+    </div>
+  );
 }
